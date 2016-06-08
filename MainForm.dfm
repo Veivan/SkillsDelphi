4d6dc66faa5d954ed163218c056f5d9758107636
veivan <veivan@yandex.ru>
2016-06-09 01:34:38 +0300

    Сделал VirtualListView. Не работает загрузка иконок.

--- MainForm.dfm
+++ MainForm.dfm
@@ -1,9 +1,9 @@
 object Form1: TForm1
-  Left = 243
-  Top = 116
-  Width = 870
-  Height = 580
+  Left = 283
+  Top = 88
   Caption = 'Explorer'
+  ClientHeight = 462
+  ClientWidth = 509
   Color = clBtnFace
   Font.Charset = DEFAULT_CHARSET
   Font.Color = clWindowText
@@ -11,101 +11,86 @@ object Form1: TForm1
   Font.Name = 'MS Sans Serif'
   Font.Style = []
   OldCreateOrder = False
+  OnCreate = FormCreate
   PixelsPerInch = 96
   TextHeight = 13
   object Splitter1: TSplitter
-    Left = 265
+    Left = 189
     Top = 41
-    Height = 505
+    Height = 421
+    ExplicitLeft = 524
+    ExplicitTop = 120
+    ExplicitHeight = 100
   end
   object Panel1: TPanel
     Left = 0
     Top = 0
-    Width = 862
+    Width = 509
     Height = 41
     Align = alTop
     TabOrder = 0
-    object ShellComboBox1: TShellComboBox
-      Left = 8
-      Top = 11
-      Width = 145
-      Height = 22
-      Root = 'rfMyComputer'
-      ShellTreeView = ShellTreeView1
-      ShellListView = ShellListView1
-      UseShellImages = True
-      DropDownCount = 8
-      TabOrder = 0
-    end
     object DriveComboBox1: TDriveComboBox
-      Left = 584
-      Top = 16
+      Left = 8
+      Top = 12
       Width = 145
       Height = 19
       DirList = DirectoryListBox1
+      TabOrder = 0
+    end
+    object Button1: TButton
+      Left = 356
+      Top = 4
+      Width = 75
+      Height = 25
+      Caption = 'Button1'
+      TabOrder = 1
+      OnClick = Button1Click
+    end
+  end
+  object Panel2: TPanel
+    Left = 0
+    Top = 41
+    Width = 189
+    Height = 421
+    Align = alLeft
+    TabOrder = 1
+    object DirectoryListBox1: TDirectoryListBox
+      Left = 4
+      Top = 48
+      Width = 145
+      Height = 97
+      FileList = FileListBox
+      TabOrder = 0
+      OnChange = DirectoryListBox1Change
+    end
+    object FileListBox: TFileListBox
+      Left = 8
+      Top = 176
+      Width = 145
+      Height = 97
+      Mask = '*.jpg;*.bmp;*.gif;*.ico'
+      ShowGlyphs = True
       TabOrder = 1
     end
   end
-  object ShellTreeView1: TShellTreeView
-    Left = 0
+  object ListView: TListView
+    Left = 192
     Top = 41
-    Width = 265
-    Height = 505
-    ObjectTypes = [otFolders]
-    Root = 'rfMyComputer'
-    ShellComboBox = ShellComboBox1
-    ShellListView = ShellListView1
-    UseShellImages = True
-    Align = alLeft
-    AutoRefresh = False
-    Indent = 19
-    ParentColor = False
-    RightClickSelect = True
-    ShowRoot = False
-    TabOrder = 1
-    OnChange = ShellTreeView1Change
-  end
-  object ShellListView1: TShellListView
-    Left = 268
-    Top = 41
-    Width = 301
-    Height = 505
-    ObjectTypes = [otNonFolders]
-    Root = 'rfMyComputer'
-    ShellTreeView = ShellTreeView1
-    ShellComboBox = ShellComboBox1
-    Sorted = True
-    OnDblClick = ShellListView1DblClick
-    ReadOnly = False
-    HideSelection = False
+    Width = 317
+    Height = 421
+    Align = alClient
+    Columns = <
+      item
+        Caption = 'Name'
+      end>
+    OwnerData = True
+    ReadOnly = True
     TabOrder = 2
+    OnData = ListViewData
+    OnDblClick = ListViewDblClick
   end
-  object FileListBox1: TFileListBox
-    Left = 584
-    Top = 160
-    Width = 145
-    Height = 97
-    ItemHeight = 16
-    Mask = '*.dcu;*.dfm'
-    ShowGlyphs = True
-    TabOrder = 3
-  end
-  object DirectoryListBox1: TDirectoryListBox
-    Left = 584
-    Top = 48
-    Width = 145
-    Height = 97
-    FileList = FileListBox1
-    ItemHeight = 16
-    TabOrder = 4
-  end
-  object Button1: TButton
-    Left = 736
-    Top = 184
-    Width = 75
-    Height = 25
-    Caption = 'Button1'
-    TabOrder = 5
-    OnClick = Button1Click
+  object ImageList: TImageList
+    Left = 108
+    Top = 45
   end
 end
--- MainForm.pas
+++ MainForm.pas
@@ -4,24 +4,32 @@ interface
 
 uses
   Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
-  Dialogs, ExtCtrls, ComCtrls, ShellCtrls, StdCtrls, FileCtrl;
+  Dialogs, ExtCtrls, ComCtrls, ShellCtrls, StdCtrls, FileCtrl,
+  ShellApi, ShlObj, ImgList;
 
 type
   TForm1 = class(TForm)
     Panel1: TPanel;
-    ShellComboBox1: TShellComboBox;
-    ShellTreeView1: TShellTreeView;
-    ShellListView1: TShellListView;
-    Splitter1: TSplitter;
-    FileListBox1: TFileListBox;
-    DirectoryListBox1: TDirectoryListBox;
     DriveComboBox1: TDriveComboBox;
+    Panel2: TPanel;
+    DirectoryListBox1: TDirectoryListBox;
+    FileListBox: TFileListBox;
+    Splitter1: TSplitter;
     Button1: TButton;
-    procedure ShellTreeView1Change(Sender: TObject; Node: TTreeNode);
-    procedure ShellListView1DblClick(Sender: TObject);
+    ListView: TListView;
+    ImageList: TImageList;
     procedure Button1Click(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+    procedure DirectoryListBox1Change(Sender: TObject);
+    procedure ListViewDblClick(Sender: TObject);
+    procedure ListViewData(Sender: TObject; Item: TListItem);
   private
     { Private declarations }
+    FIShellFolder,
+    FIDesktopFolder: IShellFolder;
+
+    procedure SetPath();
+    function GetIcon(FileName: String) : TIcon;
   public
     { Public declarations }
   end;
@@ -35,24 +43,94 @@ uses frmEditor;
 
 {$R *.dfm}
 
-procedure TForm1.ShellTreeView1Change(Sender: TObject; Node: TTreeNode);
+procedure TForm1.FormCreate(Sender: TObject);
 begin
-  //ShowMessage(Node.GetNamePath);
-  //FileListBox1.Directory := Node.GetNamePath;
+  //DirectoryListBox1.Align := alClient;
+  DirectoryListBox1.Directory := 'Z:\vmshared\1';
 end;
 
-procedure TForm1.ShellListView1DblClick(Sender: TObject);
-var fedit : TForm;
+procedure TForm1.ListViewData(Sender: TObject; Item: TListItem);
 begin
-  Application.CreateForm(TFormEditor, FormEditor);
-  FormEditor.ShowModal;
- // FileListBox1.Items;
+  if (Item.Index > FileListBox.Count) then Exit;
+  Item.Caption := FileListBox.Items.Strings[Item.Index];
+  Item.ImageIndex := Item.Index;
+  if ListView.ViewStyle <> vsReport then Exit;
+
+end;
+
+procedure TForm1.ListViewDblClick(Sender: TObject);
+begin
+  if ListView.Selected <> nil then begin
+    Application.CreateForm(TFormEditor, FormEditor);
+    FormEditor.ShowModal;
+  end;
+end;
+
+procedure TForm1.DirectoryListBox1Change(Sender: TObject);
+var i : integer; fname : string; icon : TIcon;
+begin
+  ImageList.Clear;
+  for i := 0 to FileListBox.Count - 1 do begin
+    fname := FileListBox.Directory + '\' + FileListBox.Items.Strings[i];
+//    icon := GetIcon(fname);
+//    ImageList.AddIcon(icon);
+  end;
+  ListView.LargeImages := ImageList;
+  ListView.Items.Count := FileListBox.Count;
+  SetPath();
+  ListView.Repaint;
+end;
+
+procedure TForm1.SetPath;
+begin
+  ListView.Items.BeginUpdate;
+  try
+    if ListView.Items.Count > 0 then
+    begin
+      ListView.Selected := ListView.Items[0];
+      ListView.Selected.Focused := True;
+      ListView.Selected.MakeVisible(False);
+    end;
+  finally
+    ListView.Items.EndUpdate;
+  end;
 end;
 
 procedure TForm1.Button1Click(Sender: TObject);
+var
+  icon:TIcon; w : integer;  FileName: string; h : hIcon;  IconIndex : word;
 begin
-  ShowMessage(FileListBox1.Directory + ' ' + FileListBox1.Items.Strings[0]);
+//  ShowMessage(FileListBox.Directory + ' ' + FileListBox.Items.Strings[0]);
+  FileName := FileListBox.Directory + '\' + FileListBox.Items.Strings[0];
+  icon.Handle:=ExtractIcon(Application.Handle,PChar(FileName),0);
+  IconIndex := 0;
+ h := ExtractAssociatedIcon(hInstance,
+'z:\vmshared\1\IMG_9879.JPG',
+IconIndex);
 
+icon.LoadFromFile('z:\vmshared\1\IMG_9879.JPG');
+Form1.Canvas.Draw(32, 32, icon);
+//DrawIcon(Form1.Canvas.Handle,10,10,h);
+end;
+
+function TForm1.GetIcon(FileName: String) : TIcon;
+var
+  icon:TIcon;
+  Icon32,Icon16:HICON;
+  i:integer;
+begin
+  try
+  icon.Handle:=ExtractIcon(hInstance,PChar(FileName),0);
+    i:=0;
+    while Integer(ExtractIconEx(PChar(FileName),i,Icon16,Icon32,1))>0 do begin
+      inc(i);
+      icon:=TIcon.Create;
+      icon.Handle:=Icon16;
+    end;
+  except
+    on e:Exception do
+  end;
+  Result := icon;
 end;
 
 end.
