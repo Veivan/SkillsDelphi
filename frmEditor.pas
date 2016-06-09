1c0c65c904edee350d5546bb14c1c752cbbc25bc
veivan <veivan@yandex.ru>
2016-06-09 03:47:27 +0300

    Сделал рисование в окне. Сделать отображение иконок.

--- frmEditor.pas
+++ frmEditor.pas
@@ -7,11 +7,13 @@ uses
   Dialogs, ExtCtrls, Jpeg, ExtDlgs, StdCtrls, Buttons, ToolWin, ComCtrls;
 
 type
+  TDrawKind = (None, ARect, AEllipse);
+
   TFormEditor = class(TForm)
     OpenPictureDialog1: TOpenPictureDialog;
-    CoolBar1: TCoolBar;
-    SpeedButton3: TSpeedButton;
-    SpeedButton4: TSpeedButton;
+    CoolBar: TCoolBar;
+    sbRect: TSpeedButton;
+    sbEllipse: TSpeedButton;
     Image1: TImage;
     procedure FormCreate(Sender: TObject);
     procedure Image1MouseDown(Sender: TObject; Button: TMouseButton;
@@ -20,11 +22,14 @@ type
       Shift: TShiftState; X, Y: Integer);
     procedure Image1MouseMove(Sender: TObject; Shift: TShiftState; X,
       Y: Integer);
+    procedure sbRectClick(Sender: TObject);
+    procedure sbEllipseClick(Sender: TObject);
   private
     { Private declarations }
     FFileName : string;
     AX, AY : Integer;
     doDraw : boolean;
+    DrawKind : TDrawKind;
   public
     { Public declarations }
     constructor CreateExt(AFileName: string);
@@ -46,6 +51,8 @@ end;
 procedure TFormEditor.FormCreate(Sender: TObject);
   var Pic: TPicture;
 begin
+  DrawKind := None;
+  doDraw := false;
   Pic := TPicture.Create;
   try
     Pic.LoadFromFile(FFileName);
@@ -58,25 +65,40 @@ end;
 procedure TFormEditor.Image1MouseDown(Sender: TObject;
   Button: TMouseButton; Shift: TShiftState; X, Y: Integer);
 begin
-  //Image1.Canvas.MoveTo(X,Y);
-  FormEditor.Canvas.MoveTo(X,Y);
+  if DrawKind = None then Exit;
+  doDraw := true;
+  X := X + CoolBar.Width;
   AX := X;
   AY := Y;
-  doDraw := true;
+  FormEditor.Canvas.MoveTo(AX,AY);
 end;
 
 procedure TFormEditor.Image1MouseUp(Sender: TObject; Button: TMouseButton;
   Shift: TShiftState; X, Y: Integer);
   var r : Trect;
 begin
-  //Image1.Canvas.LineTo(X,Y);
-  //Image1.Canvas.Rectangle(X,Y, X+ 50,Y + 50);
+  if DrawKind = None then Exit;
+  AX := AX - CoolBar.Width;
   r := Rect(AX,AY,X,Y);
-  //Image1.Canvas.Brush.Color := clBlack;
-  //Image1.Canvas.FrameRect(r);
-  FormEditor.Canvas.Brush.Color := clBlack;
-  FormEditor.Canvas.FrameRect(r);
+  Image1.Canvas.Pen.Color := clBlack;
+  Image1.Canvas.Brush.Style := bsClear;
+  case DrawKind of
+    ARect :
+      Image1.Canvas.Rectangle(r);
+    AEllipse :
+      Image1.Canvas.Ellipse(r);
+  end;
   doDraw := false;
+end;
+
+procedure TFormEditor.sbEllipseClick(Sender: TObject);
+begin
+  DrawKind := AEllipse;
+end;
+
+procedure TFormEditor.sbRectClick(Sender: TObject);
+begin
+  DrawKind := ARect;
 end;
 
 procedure TFormEditor.Image1MouseMove(Sender: TObject; Shift: TShiftState;
@@ -84,16 +106,20 @@ procedure TFormEditor.Image1MouseMove(Sender: TObject; Shift: TShiftState;
   var r : Trect;
 begin
   if not doDraw then Exit;
-
-//  FormEditor.Canvas.fillrect(FormEditor.Canvas.cliprect);
-//  InValidateRect(FormEditor.handle,NIL,True);
   PatBlt(FormEditor.Canvas.Handle, 0, 0,
     FormEditor.ClientWidth, FormEditor.ClientHeight, WHITENESS);
   Image1.Repaint;
 
+  X := X + CoolBar.Width;
   r := Rect(AX,AY, X,Y);
-  FormEditor.Canvas.Brush.Color := clBlack;
-  FormEditor.Canvas.FrameRect(r);
+  FormEditor.Canvas.Pen.Color := clBlack;
+  FormEditor.Canvas.Brush.Style := bsClear;
+  case DrawKind of
+    ARect :
+      FormEditor.Canvas.Rectangle(r);
+    AEllipse :
+      FormEditor.Canvas.Ellipse(r);
+  end;
 end;
 
 end.
